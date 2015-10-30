1af60866e1a0e006021d494539003729364fe3ef
SarosIndustries <h1972898@trbvm.com>
2015-10-30 13:07:15 -0600

    Update TXTFILE_UTIL.h

--- TXTFILE_UTIL.h
+++ TXTFILE_UTIL.h
@@ -1,15 +1,21 @@
 #ifndef TXTFILE_UTIL_H
 #define TXTFILE_UTIL_H
 
-typedef struct TXTFILE_DATA_NODE
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STACK_MAX 256
+
+typedef struct TXTFILE_DATA_STACK
 {
-	struct TXTFILE_DATA_NODE *NEXT;
+	int SIZE;
+	double FREQUENCY[STACK_MAX];
+	double VOLUME[STACK_MAX];
+	double LENGTH[STACK_MAX];
+	
+} TXTFILE_DATA_STACK;
 
-	float FREQUENCY;
-	float VOLUME;
-	float LENGTH;
-} TXTFILE_DATA_MASTER;
-
-TXTFILE_DATA_MASTER * parse_txtfile( char *filename );
+void stack_initialize( TXTFILE_DATA_STACK *master );
+void stack_push( TXTFILE_DATA_STACK *master, double frequency, double volume, double length );
 
 #endif
